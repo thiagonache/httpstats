0639fdd55f25489388c9183e9c7ae599bc2ba860
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2022-07-04 18:00:42 -0300

    Add missing metrics and remove commented code

--- httpstats.go
+++ httpstats.go
@@ -1,13 +1,18 @@
 package httpstats
 
 import (
+	"crypto/tls"
 	"net/http"
 	"net/http/httptrace"
 	"time"
 )
 
 type Stats struct {
-	DNS []time.Duration
+	Connect []time.Duration
+	DNS     []time.Duration
+	Send    []time.Duration
+	TLS     []time.Duration
+	Wait    []time.Duration
 }
 
 func NewHTTPStats() *Stats {
@@ -15,7 +20,13 @@ func NewHTTPStats() *Stats {
 }
 
 func (s *Stats) SetHTTPTrace(r *http.Request) *http.Request {
-	var dns time.Time
+	var (
+		connect  time.Time
+		dns      time.Time
+		send     time.Time
+		tlsStart time.Time
+		wait     time.Time
+	)
 	ct := &httptrace.ClientTrace{
 		DNSStart: func(di httptrace.DNSStartInfo) {
 			dns = time.Now()
@@ -23,92 +34,36 @@ func (s *Stats) SetHTTPTrace(r *http.Request) *http.Request {
 		DNSDone: func(di httptrace.DNSDoneInfo) {
 			s.DNS = append(s.DNS, time.Since(dns))
 		},
+		ConnectStart: func(network, addr string) {
+			connect = time.Now()
+		},
+		ConnectDone: func(network, addr string, err error) {
+			if err == nil {
+				s.Connect = append(s.Connect, time.Since(connect))
+			}
+		},
+		TLSHandshakeStart: func() {
+			tlsStart = time.Now()
+		},
+		TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
+			if err == nil {
+				s.TLS = append(s.TLS, time.Since(tlsStart))
+			}
+		},
+		WroteHeaderField: func(key string, value []string) {
+			send = time.Now()
+		},
+		WroteHeaders: func() {
+			s.Send = append(s.Send, time.Since(send))
+		},
+		WroteRequest: func(wri httptrace.WroteRequestInfo) {
+			wait = time.Now()
+		},
+		GotFirstResponseByte: func() {
+			s.Wait = append(s.Wait, time.Since(wait))
+		},
 	}
 	ctCtx := httptrace.WithClientTrace(r.Context(), ct)
 	r = r.WithContext(ctCtx)
 	return r
 }
-
-// func (s *Stats) RoundTrip(r *http.Request) (*http.Response, error) {
-// 	var (
-// 		connectStart time.Time
-// 		dnsStart     time.Time
-// 		reqStart     time.Time
-// 		sendStart    time.Time
-// 		tlsStart     time.Time
-// 		waitStart    time.Time
-// 	)
-// 	ct := &httptrace.ClientTrace{
-// 		GetConn: func(hostPort string) {
-// 			reqStart = time.Now()
-// 		},
-// 		DNSStart: func(info httptrace.DNSStartInfo) {
-// 			dnsStart = time.Now()
-// 		},
-// 		DNSDone: func(info httptrace.DNSDoneInfo) {
-// 			s.DNS = append(s.DNS, time.Since(dnsStart))
-// 		},
-// 		ConnectStart: func(network, addr string) {
-// 			connectStart = time.Now()
-// 		},
-// 		ConnectDone: func(network, addr string, err error) {
-// 			if err == nil {
-// 				s.Connect = append(s.Connect, time.Since(connectStart))
-// 			}
-// 		},
-// 		TLSHandshakeStart: func() {
-// 			tlsStart = time.Now()
-// 		},
-// 		TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
-// 			if err == nil {
-// 				s.TLS = append(s.TLS, time.Since(tlsStart))
-// 			}
-// 		},
-// 		WroteHeaderField: func(key string, value []string) {
-// 			sendStart = time.Now()
-// 		},
-// 		WroteHeaders: func() {
-// 			s.Send = append(s.Send, time.Since(sendStart))
-// 		},
-// 		WroteRequest: func(wri httptrace.WroteRequestInfo) {
-// 			waitStart = time.Now()
-// 		},
-// 		GotFirstResponseByte: func() {
-// 			s.Wait = append(s.Wait, time.Since(waitStart))
-// 		},
-// 	}
-// 	ctCtx := httptrace.WithClientTrace(r.Context(), ct)
-// 	r = r.WithContext(ctCtx)
-// 	res, err := s.next.RoundTrip(r)
-// 	transferStart := time.Now()
-// 	body, errBody := ioutil.ReadAll(res.Body)
-// 	if errBody == nil {
-// 		res.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-// 	}
-// 	s.Transfer = append(s.Transfer, time.Since(transferStart))
-// 	s.Total = append(s.Total, time.Since(reqStart))
-// 	return res, err
-// }
-
-// func (s *Stats) Get(url string) (*http.Response, error) {
-// 	c := &http.Client{Transport: s}
-// 	req, err := http.NewRequest(http.MethodGet, url, nil)
-// 	if err != nil {
-// 		return &http.Response{}, err
-// 	}
-// 	res, err := c.Do(req)
-// 	if err != nil {
-// 		return &http.Response{}, err
-// 	}
-// 	return res, nil
-// }
-
-// type Option func(*Stats)
-
-// // WithHTTPClient is the functional option to set a custom http.Client while
-// // initializing a new Stats object
-// func WithHTTPClient(client *http.Client) Option {
-// 	return func(s *Stats) {
-// 		s.client = client
-// 	}
-// }
--- httpstats_test.go
+++ httpstats_test.go
@@ -34,3 +34,79 @@ func TestSetHTTPTrace_TracksDNSTime(t *testing.T) {
 		t.Fatalf("want DNS time to be bigger than zero, got %v", dns)
 	}
 }
+
+func TestSetHTTPTrace_TracksConnectTime(t *testing.T) {
+	t.Parallel()
+	hstats := httpstats.NewHTTPStats()
+	req, err := http.NewRequest(http.MethodGet, "https://example.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req = hstats.SetHTTPTrace(req)
+	c := &http.Client{}
+	_, err = c.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	connect := hstats.Connect[0]
+	if connect <= 0 {
+		t.Fatalf("want Connect time to be bigger than zero, got %v", connect)
+	}
+}
+
+func TestSetHTTPTrace_TracksTLSTime(t *testing.T) {
+	t.Parallel()
+	hstats := httpstats.NewHTTPStats()
+	req, err := http.NewRequest(http.MethodGet, "https://example.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req = hstats.SetHTTPTrace(req)
+	c := &http.Client{}
+	_, err = c.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tls := hstats.TLS[0]
+	if tls <= 0 {
+		t.Fatalf("want TLS time to be bigger than zero, got %v", tls)
+	}
+}
+
+func TestSetHTTPTrace_TracksSendTime(t *testing.T) {
+	t.Parallel()
+	hstats := httpstats.NewHTTPStats()
+	req, err := http.NewRequest(http.MethodGet, "https://example.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req = hstats.SetHTTPTrace(req)
+	c := &http.Client{}
+	_, err = c.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	send := hstats.Send[0]
+	if send <= 0 {
+		t.Fatalf("want send time to be bigger than zero, got %v", send)
+	}
+}
+
+func TestSetHTTPTrace_TracksWaitTime(t *testing.T) {
+	t.Parallel()
+	hstats := httpstats.NewHTTPStats()
+	req, err := http.NewRequest(http.MethodGet, "https://example.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req = hstats.SetHTTPTrace(req)
+	c := &http.Client{}
+	_, err = c.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wait := hstats.Wait[0]
+	if wait <= 0 {
+		t.Fatalf("want wait time to be bigger than zero, got %v", wait)
+	}
+}
