e699a91ad47e523026b78b15ad77d2c8060c0aad
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2022-07-04 23:49:46 -0300

    Extract client trace functions as methods in Stats

--- httpstats.go
+++ httpstats.go
@@ -26,62 +26,84 @@ func NewHTTPStats() *Stats {
 	return &Stats{mu: &sync.Mutex{}}
 }
 
+func (s *Stats) dnsStart(di httptrace.DNSStartInfo) {
+	s.mu.Lock()
+	s.DNSS = time.Now()
+	s.mu.Unlock()
+}
+
+func (s *Stats) dnsDone(di httptrace.DNSDoneInfo) {
+	s.mu.Lock()
+	s.DNS = append(s.DNS, time.Since(s.DNSS))
+	s.mu.Unlock()
+}
+
+func (s *Stats) connectStart(netProto, addr string) {
+	s.mu.Lock()
+	s.ConnectS = time.Now()
+	s.mu.Unlock()
+}
+
+func (s *Stats) connectDone(netProto, addr string, err error) {
+	if err == nil {
+		s.mu.Lock()
+		s.Connect = append(s.Connect, time.Since(s.ConnectS))
+		s.mu.Unlock()
+	}
+}
+
+func (s *Stats) tlsStart() {
+	s.mu.Lock()
+	s.TLSS = time.Now()
+	s.mu.Unlock()
+}
+
+func (s *Stats) tlsDone(cs tls.ConnectionState, err error) {
+	if err == nil {
+		s.mu.Lock()
+		s.TLS = append(s.TLS, time.Since(s.TLSS))
+		s.mu.Unlock()
+	}
+}
+
+func (s *Stats) wroteHeaderField(key string, value []string) {
+	s.mu.Lock()
+	if s.SendS.IsZero() {
+		s.SendS = time.Now()
+	}
+	s.mu.Unlock()
+}
+
+func (s *Stats) wroteHeaders() {
+	s.mu.Lock()
+	s.Send = append(s.Send, time.Since(s.SendS))
+	s.mu.Unlock()
+}
+
+func (s *Stats) wroteRequest(wri httptrace.WroteRequestInfo) {
+	s.mu.Lock()
+	s.WaitS = time.Now()
+	s.mu.Unlock()
+}
+
+func (s *Stats) gotFirstResponseByte() {
+	s.mu.Lock()
+	s.Wait = append(s.Wait, time.Since(s.WaitS))
+	s.mu.Unlock()
+}
+
 func (s *Stats) SetHTTPTrace(r *http.Request) *http.Request {
 	ct := &httptrace.ClientTrace{
-		DNSStart: func(di httptrace.DNSStartInfo) {
-			s.mu.Lock()
-			s.DNSS = time.Now()
-			s.mu.Unlock()
-		},
-		DNSDone: func(di httptrace.DNSDoneInfo) {
-			s.mu.Lock()
-			s.DNS = append(s.DNS, time.Since(s.DNSS))
-			s.mu.Unlock()
-		},
-		ConnectStart: func(network, addr string) {
-			s.mu.Lock()
-			s.ConnectS = time.Now()
-			s.mu.Unlock()
-		},
-		ConnectDone: func(network, addr string, err error) {
-			if err == nil {
-				s.mu.Lock()
-				s.Connect = append(s.Connect, time.Since(s.ConnectS))
-				s.mu.Unlock()
-			}
-		},
-		TLSHandshakeStart: func() {
-			s.mu.Lock()
-			s.TLSS = time.Now()
-			s.mu.Unlock()
-		},
-		TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
-			if err == nil {
-				s.mu.Lock()
-				s.TLS = append(s.TLS, time.Since(s.TLSS))
-				s.mu.Unlock()
-			}
-		},
-		WroteHeaderField: func(key string, value []string) {
-			s.mu.Lock()
-			s.SendS = time.Now()
-			s.mu.Unlock()
-		},
-		WroteHeaders: func() {
-			s.mu.Lock()
-			s.Send = append(s.Send, time.Since(s.SendS))
-			s.mu.Unlock()
-		},
-		WroteRequest: func(wri httptrace.WroteRequestInfo) {
-			s.mu.Lock()
-			s.WaitS = time.Now()
-			s.mu.Unlock()
-		},
-		GotFirstResponseByte: func() {
-			s.mu.Lock()
-			s.Wait = append(s.Wait, time.Since(s.WaitS))
-			s.mu.Unlock()
-		},
+		DNSStart:             s.dnsStart,
+		DNSDone:              s.dnsDone,
+		ConnectStart:         s.connectStart,
+		ConnectDone:          s.connectDone,
+		TLSHandshakeStart:    s.tlsStart,
+		TLSHandshakeDone:     s.tlsDone,
+		WroteHeaderField:     s.wroteHeaderField,
+		WroteHeaders:         s.wroteHeaders,
+		WroteRequest:         s.wroteRequest,
+		GotFirstResponseByte: s.gotFirstResponseByte,
 	}
 	ctCtx := httptrace.WithClientTrace(r.Context(), ct)
 	r = r.WithContext(ctCtx)
